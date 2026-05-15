16f8a3ba34985d08db1700d99d9ed8eb2ec37239
Nimisha-NJ <117980272+Nimisha-NJ@users.noreply.github.com>
2026-05-15 19:32:09 +0530

    chore: add husky lint-staged commitlint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+    },
+    rules: {
+      "no-console": "warn",
+      "no-debugger": "error",
+    },
+  },
+];
